dc647901ab79c1f55297b66ef19147f42110fd50
Fabio Santos <fabio.santos@ifrr.edu.br>
2024-09-26 12:21:31 -0400

    Version, pyproject organization and apps.py

---
+++ src/django_govbrds/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailSeoConfig(AppConfig):
+    name = "django_govbrds"
+    verbose_name = "Django GovBR-DS"
+    default_auto_field = "django.db.models.AutoField"
